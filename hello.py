b9db575a4a035000af85de6974f54d7e67e05bba
Loading-Username <31486660+Loading-Username@users.noreply.github.com>
2017-08-30 14:20:36 -0400

    Add files via upload

---
+++ hello.py
@@ -0,0 +1,9 @@
+# This program says hello and greets a person by name.
+#
+# Derek DiCintio
+
+print("Hello")
+print("What is your name?")
+name = input()
+print("It is good to meet you, " + name + ".")
+
