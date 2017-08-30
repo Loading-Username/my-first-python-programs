2a4c0f0d23c5f994d770f48865af7e65d41643b5
Loading-Username <31486660+Loading-Username@users.noreply.github.com>
2017-08-30 14:22:18 -0400

    Add files via upload

---
+++ chatbot.py.py
@@ -0,0 +1,21 @@
+# This program is a mini chatbot.
+#
+# August 28, 2017.
+# Derek DiCintio
+
+print("Hello! What is your name?")
+name = input()
+
+print("It is good to meet you, " + name + ".")
+print("What is your favorite college?")
+college = input()
+if college == "Ohio State":
+    print("That's a great choice! They're the best!")
+elif college == "Clemson":
+    print("Ew! You are going to make me throw up, and I can't even throw up!")
+else:
+    print("That's very interesting...")
+
+print("How has your day been?")
+
+    
